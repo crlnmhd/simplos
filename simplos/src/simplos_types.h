997ffe8f1cb1504a21207686296ed212d2bad81e
Carl Nimhed <carl.nimhed@gmail.com>
2023-01-21 04:00:48 +0100

    chore: remove superfluous volatile in struct

--- simplos/src/simplos_types.h
+++ simplos/src/simplos_types.h
@@ -29,15 +29,15 @@ typedef Simplos_Task_type volatile Simplos_Task;
 typedef Simplos_Task volatile *volatile taskptr_type;
 
 typedef struct Task_Queue_type {
-  Simplos_Task_type volatile tasks[TASKS_MAX];
+  Simplos_Task_type tasks[TASKS_MAX];
   uint8_t task_index_queue[TASK_QUEUE_LENGTH];
-  uint8_t volatile curr_task_index;
+  uint8_t curr_task_index;
 } Task_Queue_type;
 typedef Task_Queue_type volatile Task_Queue;
 
 typedef struct Scheduler_type {
   Task_Queue queue;
-  volatile uint16_t os_task_sp;
+  uint16_t os_task_sp;
   bool enabled;
 } Scheduler_type;
 typedef Scheduler_type volatile Scheduler;
@@ -48,7 +48,7 @@ typedef uint16_t pid_type;
 typedef struct Kernel_type {
   Scheduler schedule;
   char task_names[TASKS_MAX][FUNCTION_NAME_MAX_LENGTH + 1];
-  volatile struct StackRange task_RAM_ranges[TASKS_MAX];
+  struct StackRange task_RAM_ranges[TASKS_MAX];
   uint32_t cs_time_counter;
   uint32_t ended_task_time_counter;
   size_t heap_start;  // dynamically determined by the RAM needed for the init
