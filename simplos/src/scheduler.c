f046f7e15be5ebdadb835b98d9fd671eab0a15b8
Carl Nimhed <carl.nimhed@gmail.com>
2023-01-25 19:27:56 +0100

    feat: use priority scheduling

--- simplos/src/scheduler.c
+++ simplos/src/scheduler.c
@@ -9,13 +9,12 @@
 #include "serial.h"
 #include "simplos.h"
 
-// TODO better scheduling. This is just a placeholder.
+uint8_t get_active_tasks(uint8_t *tasks_block_list, const uint8_t num_tasks);
+void assign_scheduler(uint8_t *task_block_list, const uint8_t starting_index,
+                      const uint8_t end_index);
+void prioritize_tasks(const uint8_t num_tasks);
 
 NO_MT void static_cyclic_scheduler(void) {
-  // This defaults to the idle task.
-  // print_schedule(kernel->schedule);
-
-  // cprint("At block: %d\n", kernel->schedule.queue.curr_task_index);
   for (uint8_t i = 1; i <= TASKS_MAX; ++i) {
     uint8_t const next_candidate_index =
         ((uint8_t)(INDEX_OF_CURRENT_TASK + i) % TASKS_MAX);
@@ -32,6 +31,15 @@ NO_MT void static_cyclic_scheduler(void) {
   if (kernel->schedule.queue.tasks[INDEX_OF_CURRENT_TASK].status == EMPTY) {
     fatal_error("No task ready to run!\n");
   }
+}
+
+void prioity_scheduler(void) {
+  BEGIN_DISCARD_VOLATILE_QUALIFIER_WARNING();
+  const uint8_t num_active_tasks =
+      get_active_tasks(kernel->schedule.queue.task_index_queue, TASKS_MAX);
+  END_DISCARD_VOLATILE_QUALIFIER_WARNING();
+
+  prioritize_tasks(num_active_tasks);
 }
 
 /*
@@ -84,14 +92,14 @@ void prioritize_tasks(const uint8_t num_tasks_in_queue) {
   }
 }
 
-void prioity_scheduler(void) {
-  ASSERT(false, "Priority schduling is not implemented!\n");
-}
-
 void select_next_task(void) {
-#if defined(PRIORITY_SCHEDULING)
-  prioity_scheduler();
-#else
-  static_cyclic_scheduler();
-#endif
+  while (kernel->schedule.queue.queue_position == 0) {
+    prioity_scheduler();
+  }
+  cprint("Scheduled tasks:");
+  kernel->schedule.queue.queue_position--;
+  INDEX_OF_CURRENT_TASK =
+      kernel->schedule.queue
+          .task_index_queue[kernel->schedule.queue.queue_position];
 }
+void schedule_tasks(void) { prioity_scheduler(); }
