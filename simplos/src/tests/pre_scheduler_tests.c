5485e49791facb0ba64897096380f97ea498b0de
Carl Nimhed <carl.nimhed@gmail.com>
2023-06-02 06:23:25 +0200

    test: pre scheduler test for SET_SP

--- simplos/src/tests/pre_scheduler_tests.c
+++ simplos/src/tests/pre_scheduler_tests.c
@@ -52,9 +52,29 @@ void verify_save_sp_macro(void) {
   task_sp = old_task_sp;  // reset
 }
 
+void verify_set_sp_macro(void) {
+  const uint16_t old_sp = SP;
+  const uint16_t old_task_sp = task_sp;
+  const uint16_t expected_new_sp = 0x123;
+
+  task_sp = expected_new_sp;
+  SET_SP();
+  ASSERT_EQ(SP, expected_new_sp, "0x%X", "Failed to set SP using SET_SP macro");
+  ASSERT_EQ(task_sp, expected_new_sp, "0x%X",
+            "task_sp has changed unexpectedly");
+
+  task_sp = old_sp;
+  SET_SP();
+  ASSERT_EQ(SP, old_sp, "0x%X", "Failed to reset stack pointer");
+  ASSERT_EQ(task_sp, old_sp, "0x%X", "Failed to reset task_sp");
+
+  task_sp = old_task_sp;  // Reset
+}
+
 void pre_scheduler_self_checks(void) {
   verify_enable_disable_mt_macros();
   verify_set_sp_to_os_sp_macro();
   verify_save_sp_macro();
+  verify_set_sp_macro();
   cprint("Pre scheduler self checks ... PASSED\n");
 }
