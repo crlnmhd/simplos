f02fb0451723a43eae2e435e10331ca68381faf1
Carl Nimhed <carl.nimhed@gmail.com>
2023-06-02 18:29:09 +0200

    feat: support checking stack integrity after test
    Check that the stack has not been obviosly corrupted by a test fucntion.

--- simplos/src/tests/pre_scheduler_tests.c
+++ simplos/src/tests/pre_scheduler_tests.c
@@ -5,6 +5,59 @@
 #include "../timers.h"
 #include "test.h"
 
+/*
+ * Push set some registers with values and check that they have not changed.
+ * */
+void run_with_stack_integrity_check(void (*test_fn)(void)) {
+  const uint8_t r16_canary = 0;
+  const uint8_t r17_canary = 33;
+  const uint8_t r18_canary = 123;
+
+  asm volatile(
+      "push r16                     \n\t"
+      "push r17                     \n\t"
+      "push r18                     \n\t"
+      "ldi  r16, %[r16_canary]      \n\t"
+      "ldi  r17, %[r17_canary]      \n\t"
+      "ldi  r18, %[r18_canary]      \n\t"
+      "push r16                     \n\t"
+      "push r17                     \n\t"
+      "push r18                     \n\t"
+      : /* No outputs */
+      : [r16_canary] "M"(r16_canary), [r17_canary] "M"(r17_canary),
+        [r18_canary] "M"(r18_canary)
+      : "r16", "r17", "r18");
+
+  test_fn();
+
+  asm goto(
+      "pop r18                      \n\t"
+      "pop r17                      \n\t"
+      "pop r16                      \n\t"
+      "cpi r16, %[r16_canary]       \n\t"
+      "brne %l[stack_error]         \n\t"
+      "cpi r17, %[r17_canary]       \n\t"
+      "brne %l[stack_error]         \n\t"
+      "cpi r18, %[r18_canary]       \n\t"
+      "brne %l[stack_error]         \n\t"
+      "pop r18                      \n\t"
+      "pop r17                      \n\t"
+      "pop r16                      \n\t"
+      "jmp %l[return_point]"  // Don't return before letting the compiler pop
+                              // the funtions variables (depending on
+                              // optimization).
+      :                       /* No outputs */
+      : [r16_canary] "M"(r16_canary), [r17_canary] "M"(r17_canary),
+        [r18_canary] "M"(r18_canary)
+      : "r16", "r17", "r18"
+      : return_point, stack_error);
+
+stack_error:
+  FATAL_ERROR("Stack integrity check failed!");
+return_point:
+  return;
+}
+
 void verify_enable_disable_mt_macros(void) {
   // Setup
   const uint8_t original_timsk1_content = TIMSK1;
