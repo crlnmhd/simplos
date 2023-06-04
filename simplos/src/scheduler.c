443d538ad8c9efb3198ab1fc335d7002b48344e0
Carl Nimhed <carl.nimhed@gmail.com>
2023-06-04 13:56:10 +0200

    refactor: rework task prioritization

--- simplos/src/scheduler.c
+++ simplos/src/scheduler.c
@@ -14,7 +14,8 @@
 uint8_t get_active_tasks(uint8_t *tasks_block_list, const uint8_t num_tasks);
 void assign_scheduler(uint8_t *task_block_list, const uint8_t starting_index,
                       const uint8_t end_index);
-void prioritize_tasks(const uint8_t num_tasks);
+void prioritize_tasks(taskptr_type tasks, const uint8_t num_tasks,
+                      volatile uint8_t *out_priority_list);
 
 void print_queue(uint8_t num_active_tasks);
 
@@ -23,8 +24,13 @@ void reschedule(void) {
   const uint8_t num_active_tasks =
       get_active_tasks(kernel->schedule.queue.task_index_queue, TASKS_MAX);
   END_DISCARD_VOLATILE_QUALIFIER_WARNING();
+  if (num_active_tasks == 0) {
+    FATAL_ERROR("Error, no tasks avalable to schedule!");
+  }
 
-  prioritize_tasks(num_active_tasks);
+  prioritize_tasks(kernel->schedule.queue.tasks, num_active_tasks,
+                   kernel->schedule.queue.task_index_queue);
+  kernel->schedule.queue.queue_position = num_active_tasks;
 #if defined(VERBOSE_OUTPUT)
   print_queue(num_active_tasks);
 #endif  // defined(VERBOSE_OUTPUT)
@@ -48,33 +54,19 @@ uint8_t get_active_tasks(uint8_t *tasks_block_list, const uint8_t num_tasks) {
 }
 
 /*
- * Reorder the task list positions [0, num_tasks) in acending order of
- * priority.
+ * Sets the priority order of the tasks queue in out_priority_list.
+ * In decreasing order of priority.
  * */
-void prioritize_tasks(const uint8_t num_tasks_in_queue) {
-  uint8_t unsorted[TASK_QUEUE_LENGTH];
-  BEGIN_DISCARD_VOLATILE_QUALIFIER_WARNING();
-  memcpy(unsorted, kernel->schedule.queue.task_index_queue, num_tasks_in_queue);
-  memset(kernel->schedule.queue.task_index_queue, 0, num_tasks_in_queue);
-  END_DISCARD_VOLATILE_QUALIFIER_WARNING();
-
-  uint8_t sorted_task_counter = 0;
-
+void prioritize_tasks(taskptr_type volatile tasks,
+                      const uint8_t num_tasks_in_queue,
+                      volatile uint8_t *out_priority_list) {
+  uint8_t num_remaining = num_tasks_in_queue;
   for (uint16_t priority = 0; priority < UINT8_MAX; priority++) {
-    for (uint8_t task_index = 0; task_index < num_tasks_in_queue;
-         task_index++) {
-      const uint16_t remaining_tasks_to_schedule =
-          (uint8_t)(num_tasks_in_queue - sorted_task_counter);
-      if (remaining_tasks_to_schedule > 0) {
-        const uint8_t task_list_index = unsorted[task_index];
-        if (kernel->schedule.queue.tasks[task_list_index].priority ==
-            priority) {
-          kernel->schedule.queue.task_index_queue[sorted_task_counter] =
-              task_list_index;
-          sorted_task_counter++;
-        }
-      } else {
-        return;
+    for (uint8_t i = 0; num_remaining > 0 && i < num_tasks_in_queue; i++) {
+      if (tasks[i].priority == priority) {
+        const int sort_index = num_tasks_in_queue - num_remaining;
+        out_priority_list[sort_index] = i;
+        num_remaining--;
       }
     }
   }
@@ -87,7 +79,8 @@ void print_queue(uint8_t num_active_tasks) {
   cprint("Start of queue:\n-------------\n");
   for (uint8_t i = 0; i < num_active_tasks; i++) {
     cprint("%d: ", i);
-    taskptr_type task_ptr = &kernel->schedule.queue.tasks[i];
+    uint8_t task_index = kernel->schedule.queue.task_index_queue[i];
+    taskptr_type task_ptr = &kernel->schedule.queue.tasks[task_index];
     print_task(task_ptr);
   }
   cprint("End of queue:\n-------------\n");
