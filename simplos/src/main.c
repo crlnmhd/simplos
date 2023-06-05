0172e44464fa1576dc4c6a331140872b275d8963
Carl Nimhed <carl.nimhed@gmail.com>
2023-06-05 09:19:53 +0200

    choe: remove untestable tests

--- simplos/src/main.c
+++ simplos/src/main.c
@@ -32,7 +32,7 @@ int main(void) {
       FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
   stdout = stdin = &uart_file;
 
-  // pre_scheduler_self_checks();
+  pre_scheduler_self_checks();
 
   /* Set up  timer interrupt for tick counting. */
   init_scheduler_interupts();
--- simplos/src/tests/pre_scheduler_tests.c
+++ simplos/src/tests/pre_scheduler_tests.c
@@ -79,58 +79,10 @@ void verify_enable_disable_mt_macros(void) {
   TIMSK1 = original_timsk1_content;  // Reset
 }
 
-void verify_set_sp_to_os_sp_macro(void) {
-  volatile uint16_t old_sp = 0, new_sp = 0;
-  old_sp = SP;
-
-  SET_SP_TO_OS_TASK_SP();
-  new_sp = SP;
-
-  ASSERT_EQ(new_sp, OS_STACK_START, "0x%X",
-            "Unexpected new stack pointer after SET_SP_TO_OS_TASK");
-
-  SP = old_sp;  // reset
-}
-
-void verify_save_sp_macro(void) {
-  const uint16_t old_task_sp = task_sp;
-  ASSERT(old_task_sp != SP,
-         "Bad test setup. Expected task_sp not to be set to SP");
-
-  SAVE_SP();
-
-  ASSERT_EQ(task_sp, SP, "0x%X",
-            "SAVE_SP macro failed to save stack pointer to task_sp variable");
-
-  task_sp = old_task_sp;  // reset
-}
-
-void verify_set_sp_macro(void) {
-  const uint16_t old_sp = SP;
-  const uint16_t old_task_sp = task_sp;
-  const uint16_t expected_new_sp = 0x123;
-
-  task_sp = expected_new_sp;
-  SET_SP();
-  ASSERT_EQ(SP, expected_new_sp, "0x%X", "Failed to set SP using SET_SP macro");
-  ASSERT_EQ(task_sp, expected_new_sp, "0x%X",
-            "task_sp has changed unexpectedly");
-
-  task_sp = old_sp;
-  SET_SP();
-  ASSERT_EQ(SP, old_sp, "0x%X", "Failed to reset stack pointer");
-  ASSERT_EQ(task_sp, old_sp, "0x%X", "Failed to reset task_sp");
-
-  task_sp = old_task_sp;  // Reset
-}
-
 void pre_scheduler_self_checks(void) {
   cprint("Running pre sheduler tests...\n");
 
   run_with_stack_integrity_check(verify_enable_disable_mt_macros);
-  run_with_stack_integrity_check(verify_set_sp_to_os_sp_macro);
-  run_with_stack_integrity_check(verify_save_sp_macro);
-  run_with_stack_integrity_check(verify_set_sp_macro);
 
   cprint("Pre scheduler self checks ... PASSED\n");
 }
