7f93985e74717f05cd2909101bc1e334c8e5d826
Carl Nimhed <carl.nimhed@gmail.com>
2024-01-20 07:05:26 +0100

    chore: remove superfluous task printing

--- simplos/src/scheduler.c
+++ simplos/src/scheduler.c
@@ -130,9 +130,6 @@ void start_scheduler(Kernel *kernel) {
     print_task(prev, kernel);
 #endif  // defined (VERBOSE_OUTPUT)
     handle_previous_task(prev, kernel);
-#if defined(VERBOSE_OUTPUT)
-    print_task(prev, kernel);
-#endif  // defined (VERBOSE_OUTPUT)
     select_next_task(kernel);
     taskptr_type next =
         &kernel->schedule.queue.tasks[INDEX_OF_CURRENT_TASK(kernel)];
