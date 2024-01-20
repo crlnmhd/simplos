23920ea3b2b3b662d29f048f51d86d270396f38f
Carl Nimhed <carl.nimhed@gmail.com>
2024-01-20 07:05:25 +0100

    feat: improve test result printout

--- simplos/src/tests/unit_test.c
+++ simplos/src/tests/unit_test.c
@@ -16,16 +16,17 @@ void run_test_suite(struct TestStatistics (*test_entry)(void), PGM_P name) {
   if (test_results.failed == 0 && test_results.skipped == 0) {
     dprintf("PASSED: all.\n");
   } else {
+    dprintf("\n--- %S ---\n", name);
     if (test_results.passed > 0) {
-      dprintf("PASSED: %d, ", test_results.passed);
+      dprintf("PASSED: %d.\n", test_results.passed);
     }
     if (test_results.skipped > 0) {
-      dprintf("SKIPPED: %d, ", test_results.skipped);
+      dprintf("SKIPPED: %d.\n", test_results.skipped);
     }
     if (test_results.failed > 0) {
-      dprintf("FAILED: %d, ", test_results.failed);
+      dprintf("FAILED: %d.\n", test_results.failed);
     }
-    dprintf("\n");
+    dprintf("\n%S\n\n", PSTR("--- end ---"));
   }
 }
 int main(void) {
