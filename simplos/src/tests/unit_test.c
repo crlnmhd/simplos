abdb9fec73b2eaa6c1a1441976c00d67ddca21cf
Carl Nimhed <carl.nimhed@gmail.com>
2024-01-20 07:05:24 +0100

    chore: make failed function status clearer

--- simplos/src/tests/unit_test.c
+++ simplos/src/tests/unit_test.c
@@ -12,10 +12,6 @@ volatile uint16_t next_task_sp = 0;
 volatile uint16_t scheduler_task_sp = 0;
 
 void run_test_suite(struct TestStatistics (*test_entry)(void), PGM_P name) {
-  dprintf("Running unit tests for: \n");
-  dprint_flash_str(name);
-  dprintf("\n");
-
   struct TestStatistics test_results = test_entry();
   if (test_results.failed > 0) {
     dprintf("FAILED: %d, ", test_results.failed);
