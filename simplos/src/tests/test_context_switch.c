97a59c4023769a02996c4eaffce6a8903a2351b7
Carl Nimhed <carl.nimhed@gmail.com>
2024-01-20 07:05:24 +0100

    test: add stack integrity check

--- simplos/src/tests/test_context_switch.c
+++ simplos/src/tests/test_context_switch.c
@@ -1,6 +1,48 @@
 #include "../hal/context_switch_macros.h"
 #include "test.h"
 #include "test_suite.h"
+/*
+ * Pushes three canaries (0xAA) onto the stack. See CHECK_STACK_CANARIES.
+ * */
+#define SETUP_STACK_CANARIES() \
+  asm volatile(                \
+      "push r16         \n\t"  \
+      "push r17         \n\t"  \
+      "push r26         \n\t"  \
+      "clr r16          \n\t"  \
+      "subi r16, -0xAA  \n\t"  \
+      "clr r17          \n\t"  \
+      "subi r17, -0xAA  \n\t"  \
+      "clr r26          \n\t"  \
+      "subi r26, -0xAA  \n\t"  \
+      "push r16         \n\t"  \
+      "push r17         \n\t"  \
+      "push r26         \n\t");
+/*
+ * Complements SETUP_STACK_CANARIES. Reads three canary bytes from the
+ * stack. Places a non-zero value in num_failuress if the read bytes do not
+ * match the expected canary values. Otherwise, num_failures is set to zero.
+ * */
+#define CHECK_STACK_CANARIES(num_failures) \
+  asm volatile(                            \
+      "pop r26                      \n\t"  \
+      "pop r17                      \n\t"  \
+      "pop r16                      \n\t"  \
+      "clr %0                       \n\t"  \
+      "cpse r26, r17                \n\t"  \
+      "inc %0                       \n\t"  \
+      "cpse r26, r16                \n\t"  \
+      "inc %0                       \n\t"  \
+      "clr r16                      \n\t"  \
+      "subi r16, -0xAA              \n\t"  \
+      "cpse r26, r16                \n\t"  \
+      "inc %0                       \n\t"  \
+      "pop 26                       \n\t"  \
+      "pop r17                      \n\t"  \
+      "pop r16                      \n\t"  \
+      : "+r"(num_failures)                 \
+      : /* No (pure) inputs */             \
+      : "r16", "r17", "r26");
 
 #define SETUP_SELECT_SCHEDULD_TASK_OR_SCHEDULER_CALL() \
   asm volatile(                                        \
@@ -23,6 +65,81 @@
       "pop r16         \n\t"                              \
       : /* No outputs*/                                   \
       :);
+
+// Tests the test helpers
+bool test_setup_and_check_of_stack_canaries_do_not_trigger_when_called_directly(
+    void) {
+  uint8_t num_failiures = 0;
+  SETUP_STACK_CANARIES();
+  CHECK_STACK_CANARIES(num_failiures);
+
+  return num_failiures == 0;
+  // Tests the test helpers
+}
+
+bool test_setup_and_check_of_stack_canaries_detect_additional_bytes_pushed(
+    void) {
+  uint8_t num_failiures = 0;
+  SETUP_STACK_CANARIES();
+  asm volatile("push r18" ::: "r18");
+  CHECK_STACK_CANARIES(num_failiures);
+  asm volatile("pop r18" ::
+                   : "r18");  // reset the offset caused by previous push
+
+  return num_failiures != 0;
+}
+
+bool test_setup_and_check_of_stack_canaries_detect_changes_to_first_canary_byte(
+    void) {
+  // First canary byte
+  uint8_t num_failiures = 0;
+
+  SETUP_STACK_CANARIES();
+  asm volatile(
+      "pop r18         \n\t"
+      "subi r18,1         \n\t"
+      "push r18         \n\t"
+      : /* No outputs*/
+      :);
+  CHECK_STACK_CANARIES(num_failiures);
+  asm volatile("" ::: "r18");
+  return num_failiures != 0;
+}
+
+bool test_setup_and_check_of_stack_canaries_detect_changes_to_second_canary_byte(
+    void) {
+  uint8_t num_failiures = 0;
+  SETUP_STACK_CANARIES();
+  asm volatile(
+      "pop r18         \n\t"
+      "pop r18         \n\t"
+      "subi r18,1       \n\t"
+      "push r18         \n\t"
+      "push r18         \n\t"
+      : /* No outputs*/
+      :);
+  CHECK_STACK_CANARIES(num_failiures);
+  asm volatile("" ::: "r18");
+  return num_failiures != 0;
+}
+bool test_setup_and_check_of_stack_canaries_detect_changes_to_third_canary_byte(
+    void) {
+  uint8_t num_failiures = 0;
+  SETUP_STACK_CANARIES();
+  asm volatile(
+      "pop r18         \n\t"
+      "pop r18         \n\t"
+      "pop r18         \n\t"
+      "subi r18,1       \n\t"
+      "push r18         \n\t"
+      "push r18         \n\t"
+      "push r18         \n\t"
+      : /* No outputs*/
+      :);
+  CHECK_STACK_CANARIES(num_failiures);
+  asm volatile("" ::: "r18");
+  return num_failiures != 0;
+}
 
 bool test_select_scheduled_task_or_reschedule_selecte_schedled_task_when_available(
     void) {
@@ -98,6 +215,26 @@ struct TestStatistics unit_test_context_switch(void) {
 
   RUN_TEST(
       test_select_scheduled_task_or_reschedule_selects_scheduler_task_when_no_task_is_scheduled,
+      &results);
+
+  RUN_TEST(
+      test_setup_and_check_of_stack_canaries_do_not_trigger_when_called_directly,
+      &results);
+
+  RUN_TEST(
+      test_setup_and_check_of_stack_canaries_detect_additional_bytes_pushed,
+      &results);
+
+  RUN_TEST(
+      test_setup_and_check_of_stack_canaries_detect_changes_to_first_canary_byte,
+      &results);
+
+  RUN_TEST(
+      test_setup_and_check_of_stack_canaries_detect_changes_to_second_canary_byte,
+      &results);
+
+  RUN_TEST(
+      test_setup_and_check_of_stack_canaries_detect_changes_to_third_canary_byte,
       &results);
 
   return results;
