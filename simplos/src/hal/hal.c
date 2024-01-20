192a3f101ec0e4112c2b7133c0cccf1b06c41883
Carl Nimhed <carl.nimhed@gmail.com>
2024-01-20 07:05:25 +0100

    chore: increase CS rate
    The exact amount is not important at the moment.

--- simplos/src/hal/hal.c
+++ simplos/src/hal/hal.c
@@ -59,7 +59,7 @@ NORETURN void halt_exec(void) {
 
 void setup_interupt_timer(__attribute__((unused)) const uint16_t frequenzy_hz) {
   const uint16_t timer_compare_match =
-      7810;  // FIXME: calcualte based on arument
+      2000;  // FIXME: calcualte based on arument
 
   TCCR1B = (1 << CS10) | (1 << CS12) |
            (1 << WGM12);        // turn on CTC mode with 1024 presclarer
