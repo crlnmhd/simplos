92223c2269e51173175583330cf76add688c503c
Carl Nimhed <carl.nimhed@gmail.com>
2024-01-20 07:05:24 +0100

    test: next_task_sp is set to next scheduled task

--- simplos/src/tests/test_scheduler.c
+++ simplos/src/tests/test_scheduler.c
@@ -13,6 +13,25 @@ bool test_select_next_task_sets_queue_positio_to_next_task_when_tasks_are_availa
            "%d");
   return true;
 }
+
+bool test_select_next_task_sets_next_task_sp_to_next_available_task(void) {
+  next_task_sp = 0xFFFF;
+  Kernel_type given_kernel = {0};
+  given_kernel.schedule.queue.queue_position = 1;
+  const uint8_t expected_queue_position = 0;
+
+  const uint8_t given_index_of_queued_task = 3;
+  given_kernel.schedule.queue.task_index_queue[expected_queue_position] =
+      given_index_of_queued_task;
+  const uint16_t expected_scheduled_task_sp_adr = 0x1234;
+  given_kernel.schedule.queue.tasks[given_index_of_queued_task].task_sp_adr =
+      expected_scheduled_task_sp_adr;
+
+  select_next_task(&given_kernel);
+  CHECK_EQ(next_task_sp, expected_scheduled_task_sp_adr, "0x%X");
+  return true;
+}
+
 struct TestStatistics unit_test_scheduler(void) {
   struct TestStatistics results = {0};
   RUN_TEST(
@@ -20,6 +39,10 @@ struct TestStatistics unit_test_scheduler(void) {
       "selects next task sets queue poisiton to next available task when tasks "
       "queued",
       &results);
+  RUN_TEST(test_select_next_task_sets_next_task_sp_to_next_available_task,
+           "selects next task sets \'next_task_sp\' to next queied tasks SP's "
+           "address",
+           &results);
 
   return results;
 }
