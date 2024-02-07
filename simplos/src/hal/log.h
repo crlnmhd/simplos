1288a4a0a379f22581a971601822bb776154e01b
Carl Nimhed <carl.nimhed@gmail.com>
2024-02-07 16:46:40 +0100

    refactor: remove unused function
    It's a method now

--- simplos/src/hal/log.h
+++ simplos/src/hal/log.h
@@ -21,6 +21,5 @@ class Log {
   size_t num_buffer_bytes_remaining;
 };
 
-bool add_log_entry(Log *log, const char *entry);
 bool log_contains_entry(Log *log, const char *expected_entry);
 #endif  // LOG_H_
