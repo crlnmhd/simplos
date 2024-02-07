7038a01bc8df3f5658071c9551a7472c6294a4e1
Carl Nimhed <carl.nimhed@gmail.com>
2024-02-07 17:18:04 +0100

    test: avoid testing thigs the interface doesn't expose

--- simplos/src/tests/test_log.c
+++ simplos/src/tests/test_log.c
@@ -28,25 +28,6 @@ bool test_decreases_remaining_buffer_bytes_by_size_of_mesage_and_terminator(
   return TEST_PASSED;
 }
 
-bool test_adds_message_to_the_end_of_the_given_buffer(void) {
-  const uint8_t sufficient_buffer_size = 10;
-  char buf[sufficient_buffer_size];
-  Log log{buf, sufficient_buffer_size};
-  const size_t expected_message_lenght = 4;
-  const char *message = "Hej";  // 4 bytes including null terminator.
-
-  CHECK_TRUE(log.add_entry(message));
-
-  const int16_t memcmp_buffer_and_expected_string =
-      memcmp(log.buffer, message, expected_message_lenght);
-  const bool buffer_starts_with_expected_message =
-      (memcmp_buffer_and_expected_string == 0);
-
-  CHECK_TRUE(buffer_starts_with_expected_message);
-
-  return TEST_PASSED;
-}
-
 bool test_log_contains_entry_finds_entry_in_buffer(void) {
   const uint16_t buffer_size = 10;
   char buffer[buffer_size];
@@ -102,8 +83,6 @@ struct TestStatistics unit_test_hal_log(void) {
   RUN_TEST(
       test_decreases_remaining_buffer_bytes_by_size_of_mesage_and_terminator,
       &test_resutls);
-
-  RUN_TEST(test_adds_message_to_the_end_of_the_given_buffer, &test_resutls);
 
   RUN_TEST(test_can_add_multiple_messages, &test_resutls);
 
