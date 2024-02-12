d44eb6291ed9cc5482692f6d102081ca2a600796
Carl Nimhed <carl.nimhed@gmail.com>
2024-02-12 08:15:11 +0100

    refactor: move test of task name setting

--- simplos/src/tests/test_kernel.cpp
+++ simplos/src/tests/test_kernel.cpp
@@ -11,27 +11,11 @@ bool test_kernel_data_is_initilized_with_pid_counter_set_to_zero() {
   return TEST_PASSED;
 }
 
-bool test_set_name_sets_name_of_task() {
-  Kernel kernel{};
-  constexpr uint8_t task_index{3};
-  constexpr char expected_task_name[] = "Task 1";
-  Simplos_Task *task = &kernel.schedule.queue.tasks[task_index];
-  task->set_name(progmem_string("Task 1"));
-
-  const bool task_name_is_equal_to_expected =
-      !strcmp_P(expected_task_name,
-                task->name.progmem_str);  // bounded strncmp  not available
-
-  CHECK_TRUE(task_name_is_equal_to_expected);
-  return TEST_PASSED;
-}
-
 struct TestStatistics unit_test_kernel_status() {
   struct TestStatistics results {};
 
   RUN_TEST(test_kernel_data_is_initilized_with_pid_counter_set_to_zero,
            &results);
 
-  RUN_TEST(test_set_name_sets_name_of_task, &results);
   return results;
 }
--- simplos/src/tests/test_tasks.cpp
+++ simplos/src/tests/test_tasks.cpp
@@ -9,10 +9,26 @@ bool test_task_is_initilized_with_status_empty() {
   return TEST_PASSED;
 }
 
+bool test_set_name_sets_name_of_task() {
+  Kernel kernel{};
+  constexpr char expected_task_name[] = "Task 1";
+  Simplos_Task task{};
+  task.set_name(progmem_string("Task 1"));
+
+  const bool task_name_is_equal_to_expected =
+      !strcmp_P(expected_task_name,
+                task.name.progmem_str);  // bounded strncmp  not available
+
+  CHECK_TRUE(task_name_is_equal_to_expected);
+  return TEST_PASSED;
+}
+
 struct TestStatistics unit_test_tasks() {
   struct TestStatistics results {};
 
   RUN_TEST(test_task_is_initilized_with_status_empty, &results)
+
+  RUN_TEST(test_set_name_sets_name_of_task, &results);
 
   return results;
 }
