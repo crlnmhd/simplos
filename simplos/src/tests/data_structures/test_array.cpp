174d1bdaabdfa2e4bf82c8513a04b2f1a41f0c70
Carl Nimhed <carl.nimhed@gmail.com>
2024-02-27 21:11:51 +0100

    test: test array OOB checking

--- simplos/src/tests/data_structures/test_array.cpp
+++ simplos/src/tests/data_structures/test_array.cpp
@@ -5,11 +5,26 @@
 #include "../test_suite.hpp"
 
 bool test_can_initiate_array_with_default_values() {
+  constexpr size_t given_size{3};
+  Array<uint8_t, given_size> arr;
+
+  CHECK_TRUE(arr.at(0).is_valid());
+  CHECK_EQ(*arr.at(0), 0, "%lu");
+
+  CHECK_TRUE(arr.at(1).is_valid());
+  CHECK_EQ(*arr.at(1), 0, "%lu");
+
+  CHECK_TRUE(arr.at(2).is_valid());
+  CHECK_EQ(*arr.at(2), 0, "%lu");
+
+  return TEST_PASSED;
+}
+
+bool test_returns_optional_holding_invalid_when_index_is_out_of_bounds() {
   constexpr size_t given_size{5};
   Array<uint8_t, given_size> arr;
 
-  CHECK_TRUE(arr.at(4).is_valid());
-  CHECK_EQ(*arr.at(4), 0, "%lu");
+  CHECK_FALSE(arr.at(given_size).is_valid());
 
   return TEST_PASSED;
 }
@@ -18,6 +33,9 @@ TestStatistics unit_test_data_structures() {
   TestStatistics results;
 
   RUN_TEST(test_can_initiate_array_with_default_values, results);
+
+  RUN_TEST(test_returns_optional_holding_invalid_when_index_is_out_of_bounds,
+           results);
 
   return results;
 }
